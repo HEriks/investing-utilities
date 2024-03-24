5d0f7d3aef57753cfa6279ea78db4008859c4740
HEriks <34511737+HEriks@users.noreply.github.com>
2024-03-24 22:34:53 +0100

    Update companies.csv and present_estimates.py

--- MoS-Calculator/present_estimates.py
+++ MoS-Calculator/present_estimates.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 import pandas as pd
+from pandasgui import show
 from get_latest_price_data import get_latest_price_data
 
 def get_tickers(dataframe : pd.DataFrame) -> list:
@@ -30,38 +31,13 @@ def fetch_data():
     df['motivatedPrice2025'] = df.apply(lambda row: row.eps2025e * row.motivatedPE, axis=1)
 
     # Calculate margin of safety for 2024 and 2025
-    df['MoS2024'] = 1 - df['motivatedPrice2024'] / df['currentPrice']
-    df['MoS2025'] = 1 - df['motivatedPrice2025'] / df['currentPrice']
+    df['MoS2024'] = round(1 - df['currentPrice'] / df['motivatedPrice2024'], 2) 
+    df['MoS2025'] = round(1 - df['currentPrice'] / df['motivatedPrice2025'], 2) 
 
     return df
 
 def present_data(dataframe : pd.DataFrame) -> None:
-    # Create a GUI window
-    window = tk.Tk()
-    window.title("Margin of Safety")
-
-    # Create a DataFrame to store the margin of safety data
-    mos_data = df[['ticker', 'MoS2024', 'MoS2025']]
-
-    # Create a Treeview widget to display the data
-    tree = ttk.Treeview(window)
-    tree["columns"] = ("MoS2024", "MoS2025")
-    tree.heading("#0", text="Ticker")
-    tree.heading("MoS2024", text="MoS 2024")
-    tree.heading("MoS2025", text="MoS 2025")
-
-    # Insert the data into the Treeview widget
-    for index, row in mos_data.iterrows():
-        ticker = row['ticker']
-        mos_2024 = row['MoS2024']
-        mos_2025 = row['MoS2025']
-        tree.insert("", "end", text=ticker, values=(mos_2024, mos_2025))
-
-    # Add the Treeview widget to the window
-    tree.pack()
-
-    # Start the GUI event loop
-    window.mainloop()
+    show(dataframe)
 
 def main():
     # Call the fetch_data function to fetch the data and present it in a GUI
